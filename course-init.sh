1fc14f7b15e4eb46f6b5baa0513be28d77a0f5f5
wurquhart <wurquhar@redhat.com>
2024-08-08 14:50:03 -0400

    Updated titles

--- course-init.sh
+++
@@ -1,8 +0,0 @@
-REPONAME=$(basename `git rev-parse --show-toplevel`)
-echo -ne "Initializing $REPONAME . . . "
-sed -i "s/REPLACEREPONAME/${REPONAME}/g" *.* > /dev/null 2>&1
-##TODO: Any automation required for repo customization (like for example HOL vs BFX) goes here.
-echo -e "done"
-rm -f $0
-echo -e "\nPlease replace the specified strings in the files below and commit the changes before proceeding with the course development."
-grep REPLACE *.*
